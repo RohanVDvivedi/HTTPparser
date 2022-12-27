f51519ee49c6ce112617f862943bfaf3b2542b5e
RohanVDvivedi <rohandvivedi@gmail.com>
2022-12-27 15:46:00 -0700

    dstring map to dmap

--- inc/dmap.h
+++ inc/dmap.h
@@ -4,11 +4,19 @@
 typedef struct dmap_entry dmap_entry;
 struct dmap_entry
 {
+	unsigned int hashvalue;
 	dstring key;
 
-	unsigned int values_count;
-	dstring* values;
+	array values;
 };
+
+void init_dmap_entry(dmap_entry* dmap_entry_p);
+
+void insert_in_dmap_entry(dmap_entry* dmap_entry_p, const dstring* value);
+
+void deinit_dmap_entry(dmap_entry* dmap_entry_p);
+
+
 
 typedef hashmap dmap;
 
@@ -16,10 +24,11 @@ void init_dmap(dmap* dmap_p, int case_in_sensitive_key);
 
 dmap_entry* get_from_dmap(const dmap* dmap_p, const dstring* key);
 
+// inserts a new dmap_entry in dmap with the given key and returns the inserted dmap
 dmap_entry* insert_in_dmap(const dmap* dmap_p, const dstring* key);
 
 int delete_from_dmap(const dmap* dmap_p, const dstring* key);
 
-void deinit_dmap(dmap* dmap_p, int case_in_sensitive_key);
+void deinit_dmap(dmap* dmap_p);
 
 #endif
