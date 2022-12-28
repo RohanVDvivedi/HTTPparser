8c93c44c065be2617557a6b63d7db9f704ecfe67
RohanVDvivedi <rohandvivedi@gmail.com>
2022-12-28 00:03:24 -0700

    defined http_request structure

--- inc/http_request.h
+++ inc/http_request.h
@@ -1,4 +1,25 @@
 #ifndef HTTP_REQUEST_H
 #define HTTP_REQUEST_H
 
+#include<http_method.h>
+#include<dmap.h>
+
+typedef struct http_request http_request;
+struct http_request
+{
+	http_method method;
+
+	dstring path;
+
+	dmap path_params;
+
+	// version number
+	int major;
+	int minor;
+
+	dmap headers;
+
+	// body stream
+};
+
 #endif
