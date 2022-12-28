36e12f47e3943976833636070f6fc2fc016af833
RohanVDvivedi <rohandvivedi@gmail.com>
2022-12-28 00:42:39 -0700

    added definition for http response object

--- inc/http_response.h
+++ inc/http_response.h
@@ -1,4 +1,21 @@
 #ifndef HTTP_RESPONSE_H
 #define HTTP_RESPONSE_H
 
+#include<http_method.h>
+#include<dmap.h>
+
+typedef struct http_response http_response;
+struct http_response
+{
+	// version number
+	int major;
+	int minor;
+
+	int status;
+
+	dmap headers;
+
+	// body stream
+};
+
 #endif
