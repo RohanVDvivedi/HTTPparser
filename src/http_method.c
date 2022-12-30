09dbc35ec0a5af3a25f06ef0247cc736888adac7
RohanVDvivedi <rohandvivedi@gmail.com>
2022-12-30 14:20:00 -0700

    implemented serialization method for http method

--- src/http_method.c
+++ src/http_method.c
@@ -11,3 +11,16 @@ const char* http_method_strings[] = {
 	"TRACE",
 	"CONNECT"
 };
+
+int parse_http_method(stream* rs, http_method* m);
+
+int serialize_http_method(stream* ws, const http_method* m)
+{
+	int error = 0;
+
+	write_to_stream(ws, http_method_strings[(*m)], strlen(http_method_strings[(*m)]), &error);
+	if(error)
+		return -1;
+
+	return 0;
+}
