68d06b54bde07df67cd30704265abfa160de0cf3
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-09 20:49:07 -0700

    fixed dmap now dmap is similar to a unordered_multimap

--- inc/dmap.h
+++ inc/dmap.h
@@ -8,15 +8,13 @@ typedef struct dmap_entry dmap_entry;
 struct dmap_entry
 {
 	dstring key;
+	dstring value;
 
-	unsigned int values_capacity;
-	unsigned int values_count;
-	dstring* values;
+	// this is the node that will be used by the dmap
+	rbhnode embed_node;
 };
 
-void init_dmap_entry(dmap_entry* dmap_entry_p, const dstring* key);
-
-void insert_in_dmap_entry(dmap_entry* dmap_entry_p, const dstring* value);
+void init_dmap_entry(dmap_entry* dmap_entry_p, const dstring* key, const dstring* value);
 
 void deinit_dmap_entry(dmap_entry* dmap_entry_p);
 
@@ -30,9 +28,9 @@ dmap_entry* get_from_dmap(const dmap* dmap_p, const dstring* key);
 
 // inserts a new dmap_entry in dmap with the given key and returns the inserted dmap_entry
 // if an entry with the given key already exists then, no insertion will be performed and the existing entry will be returned
-dmap_entry* insert_in_dmap(dmap* dmap_p, const dstring* key);
+dmap_entry* insert_in_dmap(dmap* dmap_p, const dstring* key, const dstring* value);
 
-int delete_from_dmap(dmap* dmap_p, const dstring* key);
+int delete_from_dmap(dmap* dmap_p, const dmap_entry* dmap_entry_p);
 
 void deinit_dmap(dmap* dmap_p);
 
--- src/dmap.c
+++ src/dmap.c
@@ -3,31 +3,17 @@
 #include<stdlib.h>
 #include<ctype.h>
 
-void init_dmap_entry(dmap_entry* dmap_entry_p, const dstring* key)
+void init_dmap_entry(dmap_entry* dmap_entry_p, const dstring* key, const dstring* value)
 {
 	init_copy_dstring(&(dmap_entry_p->key), key);
-	dmap_entry_p->values_capacity = 0;
-	dmap_entry_p->values_count = 0;
-	dmap_entry_p->values = NULL;
-}
-
-void insert_in_dmap_entry(dmap_entry* dmap_entry_p, const dstring* value)
-{
-	if(dmap_entry_p->values_count == dmap_entry_p->values_capacity)
-	{
-		dmap_entry_p->values_capacity = 2 * dmap_entry_p->values_capacity + 1;
-		dmap_entry_p->values = realloc(dmap_entry_p->values, dmap_entry_p->values_capacity * sizeof(dstring));
-	}
-	init_copy_dstring(dmap_entry_p->values + dmap_entry_p->values_count++, value);
+	init_copy_dstring(&(dmap_entry_p->value), value);
+	initialize_rbhnode(&(dmap_entry_p->embed_node));
 }
 
 void deinit_dmap_entry(dmap_entry* dmap_entry_p)
 {
 	deinit_dstring(&(dmap_entry_p->key));
-	for(unsigned int i = 0; i < dmap_entry_p->values_count; i++)
-		deinit_dstring(dmap_entry_p->values + i);
-	if(dmap_entry_p->values)
-		free(dmap_entry_p->values);
+	deinit_dstring(&(dmap_entry_p->value));
 }
 
 static unsigned int dstring_key_hash_case(const void* element)
@@ -55,9 +41,9 @@ static unsigned int dstring_key_hash(const void* element)
 void init_dmap(dmap* dmap_p, int ignore_case_for_key)
 {
 	if(ignore_case_for_key)
-		initialize_hashmap(dmap_p, ROBINHOOD_HASHING, 8, dstring_key_hash_case, (int (*)(const void*, const void*))case_compare_dstring, 0);
+		initialize_hashmap(dmap_p, ROBINHOOD_HASHING, 8, dstring_key_hash_case, (int (*)(const void*, const void*))case_compare_dstring, offsetof(dmap_entry, embed_node));
 	else
-		initialize_hashmap(dmap_p, ROBINHOOD_HASHING, 8, dstring_key_hash, (int (*)(const void*, const void*))compare_dstring, 0);
+		initialize_hashmap(dmap_p, ROBINHOOD_HASHING, 8, dstring_key_hash, (int (*)(const void*, const void*))compare_dstring, offsetof(dmap_entry, embed_node));
 }
 
 dmap_entry* get_from_dmap(const dmap* dmap_p, const dstring* key)
@@ -65,35 +51,31 @@ dmap_entry* get_from_dmap(const dmap* dmap_p, const dstring* key)
 	return (dmap_entry*) find_equals_in_hashmap(dmap_p, &((dmap_entry){.key = (*key)}));
 }
 
-dmap_entry* insert_in_dmap(dmap* dmap_p, const dstring* key)
+dmap_entry* insert_in_dmap(dmap* dmap_p, const dstring* key, const dstring* value)
 {
-	dmap_entry* e = get_from_dmap(dmap_p, key);
-	if(e == NULL)
-	{
-		e = malloc(sizeof(dmap_entry));
-		init_dmap_entry(e, key);
+	dmap_entry* dmap_entry_p = malloc(sizeof(dmap_entry));
+	init_dmap_entry(dmap_entry_p, key, value);
 
-		// insert the new dmap_entry in hashmap
-		// if it fails then expand the hashmap and retry insertion
-		if(!insert_in_hashmap(dmap_p, e))
-		{
-			expand_hashmap(dmap_p, 2.0);
-			insert_in_hashmap(dmap_p, e);
-		}
+	// insert the new dmap_entry in hashmap
+	// if it fails then expand the hashmap and retry insertion
+	if(!insert_in_hashmap(dmap_p, dmap_entry_p))
+	{
+		expand_hashmap(dmap_p, 2.0);
+		insert_in_hashmap(dmap_p, dmap_entry_p);
 	}
-	return e;
+
+	return dmap_entry_p;
 }
 
-int delete_from_dmap(dmap* dmap_p, const dstring* key)
+int delete_from_dmap_key(dmap* dmap_p, dmap_entry* dmap_entry_p)
 {
-	dmap_entry* e = get_from_dmap(dmap_p, key);
-	if(e == NULL)
-		return 0;
-
-	remove_from_hashmap(dmap_p, e);
-	deinit_dmap_entry(e);
-	free(e);
-	return 1;
+	if(remove_from_hashmap(dmap_p, dmap_entry_p))
+	{
+		deinit_dmap_entry(dmap_entry_p);
+		free(dmap_entry_p);
+		return 1;
+	}
+	return 0;
 }
 
 static void destroy_dmap_entries_in_dmap(const void* data, const void* additional_params)
