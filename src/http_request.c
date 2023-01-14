fab9fcfb27013b14bf363d7cc6c152cfb5372955
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-13 23:20:37 -0700

    implemented parse head for request and response

--- src/http_request.c
+++ src/http_request.c
@@ -13,3 +13,36 @@ void deinit_http_request(http_request* hr_p)
 	deinit_dmap(&(hr_p->path_params));
 	deinit_dmap(&(hr_p->headers));
 }
+
+#include<http_request_line.h>
+#include<http_headers.h>
+
+int parse_http_request_head(stream* rs, http_request* hr_p)
+{
+	int error = 0;
+
+	error = parse_http_request_line(rs, hr_p);
+	if(error)
+		return -1;
+
+	error = parse_http_headers(rs, &(hr_p->headers));
+	if(error)
+		return -1;
+
+	return 0;
+}
+
+int serialize_http_request_head(stream* ws, const http_request* hr_p)
+{
+	int error = 0;
+
+	error = serialize_http_request_line(ws, hr_p);
+	if(error)
+		return -1;
+
+	error = serialize_http_headers(ws, &(hr_p->headers));
+	if(error)
+		return -1;
+
+	return 0;
+}
--- src/http_response.c
+++ src/http_response.c
@@ -9,3 +9,36 @@ void deinit_http_response(http_response* hr_p)
 {
 	deinit_dmap(&(hr_p->headers));
 }
+
+#include<http_response_line.h>
+#include<http_headers.h>
+
+int parse_http_response_head(stream* rs, http_response* hr_p)
+{
+	int error = 0;
+
+	error = parse_http_response_line(rs, hr_p);
+	if(error)
+		return -1;
+
+	error = parse_http_headers(rs, &(hr_p->headers));
+	if(error)
+		return -1;
+
+	return 0;
+}
+
+int serialize_http_request_head(stream* ws, const http_response* hr_p)
+{
+	int error = 0;
+
+	error = serialize_http_response_line(ws, hr_p);
+	if(error)
+		return -1;
+
+	error = serialize_http_headers(ws, &(hr_p->headers));
+	if(error)
+		return -1;
+
+	return 0;
+}
