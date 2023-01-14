5205aa742d50228b621c6bdde305a32932bb888e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-13 23:32:39 -0700

    implemented serialize function for http headers

--- src/http_headers.c
+++ src/http_headers.c
@@ -1,5 +1,9 @@
 #include<http_headers.h>
 
+#include<stream_util.h>
+
+#include<dstring.h>
+
 int parse_http_headers(stream* rs, dmap* headers)
 {
 	// TODO
@@ -7,5 +11,15 @@ int parse_http_headers(stream* rs, dmap* headers)
 
 int serialize_http_headers(stream* ws, const dmap* headers)
 {
-	// TODO
+	int error = 0;
+
+	for(const dmap_entry* e = get_first_of_in_hashmap(headers, FIRST_OF_HASHMAP); e != NULL; e = get_next_of_in_hashmap(headers, e, ANY_IN_HASHMAP))
+		write_to_stream_formatted(ws, printf_dstring_format ": " printf_dstring_format "\r\n", &error, printf_dstring_params(&(e->key)), printf_dstring_params(&(e->value)));
+
+	static const char* CRLF = "\r\n";
+	write_to_stream(ws, CRLF, 2, &error);
+	if(error)
+		return -1;
+
+	return 0;
 }
