872369818c906f68cd44d7f2cc5ffa7be9a53468
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-21 20:58:57 -0700

    started with intializing body stream functions

--- src/http_body_stream.c
+++ src/http_body_stream.c
@@ -22,10 +22,22 @@ static void destroy_stream_context_body_stream(void* stream_context)
 
 int initialize_readable_body_stream(stream* strm, stream* underlying_stream, const dmap* headers)
 {
-	// TODO
+	body_stream_context* stream_context = malloc(sizeo(body_stream_context));
+
+	// intialize stream context
+
+	initialize_stream(strm, stream_context, read_body_from_stream_body, NULL, close_stream_context_body_stream, destroy_stream_context_body_stream);
+
+	return 1;
 }
 
 int initialize_writable_body_stream(stream* strm, stream* underlying_stream, const dmap* headers)
 {
-	// TODO
+	body_stream_context* stream_context = malloc(sizeo(body_stream_context));
+
+	// intialize stream context
+
+	initialize_stream(strm, stream_context, NULL, write_body_to_stream_body, close_stream_context_body_stream, destroy_stream_context_body_stream);
+
+	return 1;
 }
