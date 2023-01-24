f8a71d55cbcbc74599755e25b64df7087e1ade11
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-23 20:05:27 -0700

    test setup completed we are able to make a connection to http server

--- test/test.c
+++ test/test.c
@@ -1,68 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include<http_request_line.h>
-#include<http_response_line.h>
+#include<http_request.h>
+#include<http_response.h>
+#include<http_body_stream.h>
 
-#include<file_descriptor_stream.h>
-
-#define TEST_REQUEST
-//#define TEST_RESPONSE
+#include<comm_address.h>
+#include<client.h>
+#include<ssl_ctx_helper.h>
 
 int main()
 {
-	stream rs, ws;
-	initialize_stream_for_fd(&rs, 0);
-	initialize_stream_for_fd(&ws, 1);
-	
-	#if defined TEST_REQUEST
+	http_request hrq;
+	init_http_request(&hrq);
+	hrq.method = GET;
+	concatenate_dstring(&(hrq.path), &get_literal_cstring("/api/v2/entries/en/hello"));
+	hrq.version.major = 1;
+	hrq.version.minor = 1;
+	insert_in_dmap(&(hrq.headers), &(get_literal_cstring("host")), &(get_literal_cstring("api.dictionaryapi.dev")));
+	insert_in_dmap(&(hrq.headers), &(get_literal_cstring("accept")), &(get_literal_cstring("*/*")));
+
+	http_response hrp;
+	init_http_response(&hrp);
+
+	comm_address server_address;
+	int res = lookup_by_name("api.dictionaryapi.dev", /*"443"*/ "80", SOCK_STREAM, AF_INET, &server_address, 1);
+	if(res == 0)
 	{
-		http_request hr;
-		init_http_request(&hr);
-
-		if(parse_http_request_head(&rs, &hr))
-		{
-			printf("parse error\n");
-			return 0;
-		}
-
-		printf("path : " printf_dstring_format "\n", printf_dstring_params(&(hr.path)));
-		printf("params : \n");
-		print_dmap(&ws, &(hr.path_params));
-		printf("headers : \n");
-		print_dmap(&ws, &(hr.headers));
-
-		if(serialize_http_request_head(&ws, &hr))
-		{
-			printf("serliazation error\n");
-			return 0;
-		}
-
-		deinit_http_request(&hr);
+		printf("no servers found\n");
+		goto EXIT_1;
 	}
-	#elif defined TEST_RESPONSE
+
+	stream raw_stream;
+	//ssl_lib_init();
+	SSL_CTX* ssl_ctx = NULL;//get_ssl_ctx_for_client(NULL, NULL);
+	if(!make_connection_stream(&raw_stream, &server_address, NULL, ssl_ctx))
 	{
-		http_response hr;
-		init_http_response(&hr);
-
-		if(parse_http_response_head(&rs, &hr))
-		{
-			printf("parse error\n");
-			return 0;
-		}
-
-		if(serialize_http_response_head(&ws, &hr))
-		{
-			printf("serliazation error\n");
-			return 0;
-		}
-
-		deinit_http_response(&hr);
+		printf("failed to make connection");
+		goto EXIT_2;
 	}
-	#endif
 
-	deinitialize_stream(&rs);
-	deinitialize_stream(&ws);
+	int error = 0;
+
+	close_stream(&raw_stream, &error);
+	deinitialize_stream(&raw_stream);
+
+	EXIT_2:;
+	if(ssl_ctx != NULL)
+		destroy_ssl_ctx(ssl_ctx);
+
+	EXIT_1:;
+	deinit_http_request(&hrq);
+	deinit_http_response(&hrp);
 
 	return 0;
 }
--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,1 +1,1 @@
-gcc ./test.c -o test.out -lhttpparser -lconnman -lcutlery
+gcc ./test.c -o test.out -lhttpparser -lconnman -lcutlery -lssl
