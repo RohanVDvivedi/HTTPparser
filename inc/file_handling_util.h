0b93dd589c7312dfd0f1bf5150ecbb9113c7b1ca
RohanVDvivedi <rohandvivedi@gmail.com>
2023-02-15 22:56:37 -0700

    added minor comments

--- inc/file_handling_util.h
+++ inc/file_handling_util.h
@@ -4,6 +4,7 @@
 #include<dstring.h>
 
 // it returns POINT_DSTR (pointing to content of file_path) hence you do not need to deinit the return value
+// in absence of '.' chanracter an empty dstring is returned
 dstring get_extension_from_file_path(const dstring* file_path);
 
 #endif
