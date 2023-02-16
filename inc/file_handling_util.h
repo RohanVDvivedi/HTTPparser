efd8bf97dd33f36630eb5a76d5e18ecb74c7d85e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-02-15 22:55:51 -0700

    minor change

--- inc/file_handling_util.h
+++ inc/file_handling_util.h
@@ -1,6 +1,8 @@
 #ifndef FILE_HANDLING_UTIL_H
 #define FILE_HANDLING_UTIL_H
 
+#include<dstring.h>
+
 // it returns POINT_DSTR (pointing to content of file_path) hence you do not need to deinit the return value
 dstring get_extension_from_file_path(const dstring* file_path);
 
