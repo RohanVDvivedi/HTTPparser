ecd06d4f3301144e627b7b3e50e3dfa71023d12b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-02-15 23:02:16 -0700

    added logic to test new functionality in file_handling_util

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,5 @@
 gcc ./test.c -o test.out -lhttpparser -lconnman -lcutlery -lssl
 
 gcc ./test_c.c -o test_c.out -lhttpparser -lconnman -lcutlery -lssl
+
+gcc ./test_file_handling_util.c -o test_file_handling_util.out -lhttpparser -lcutlery
