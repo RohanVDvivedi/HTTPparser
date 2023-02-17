c4f6f133fb972f5de0c7f90ecf889339e32fac96
RohanVDvivedi <rohandvivedi@gmail.com>
2023-02-16 21:47:51 -0700

    added logic to get mimetype from extension

--- test/test_file_handling_util.c
+++ test/test_file_handling_util.c
@@ -4,11 +4,17 @@
 
 int main()
 {
-	dstring file_path = get_dstring_pointing_to_literal_cstring("/main/test.xhtml");
+	dstring file_path = get_dstring_pointing_to_literal_cstring("/main/test..mpeg");
 	dstring file_ext = get_extension_from_file_path(&file_path);
 	printf(printf_dstring_format "\n", printf_dstring_params(&file_ext));
+	dstring mimetype = get_mimetype_from_file_extension(&file_ext);
+	printf(printf_dstring_format "\n", printf_dstring_params(&mimetype));
+
+	printf("\n");
 
 	file_path = get_dstring_pointing_to_literal_cstring("/main/test_xhtml");
 	file_ext = get_extension_from_file_path(&file_path);
 	printf(printf_dstring_format "\n", printf_dstring_params(&file_ext));
+	mimetype = get_mimetype_from_file_extension(&file_ext);
+	printf(printf_dstring_format "\n", printf_dstring_params(&mimetype));
 }
