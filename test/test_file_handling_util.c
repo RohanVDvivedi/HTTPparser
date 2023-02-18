ec77c3563fd4762d305fc16da1788abc2e7829b4
RohanVDvivedi <rohandvivedi@gmail.com>
2023-02-17 18:44:52 -0700

    added test cases for matching paths with regex

--- test/test_file_handling_util.c
+++ test/test_file_handling_util.c
@@ -21,4 +21,36 @@ int main()
 	dstring accept       = get_dstring_pointing_to_literal_cstring("application/*");
 	dstring content_type = get_dstring_pointing_to_literal_cstring("text/plain");
 	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&accept), printf_dstring_params(&content_type), match_accept_to_content_type(&content_type, &accept));
+
+	dstring path_regex = get_dstring_pointing_to_literal_cstring("");
+	dstring path       = get_dstring_pointing_to_literal_cstring("");
+	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&path_regex), printf_dstring_params(&path), match_path_with_path_regex(&path, &path_regex));
+
+	path_regex = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi");
+	path       = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi/roopa/vipul");
+	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&path_regex), printf_dstring_params(&path), match_path_with_path_regex(&path, &path_regex));
+
+	path_regex = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi");
+	path       = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar");
+	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&path_regex), printf_dstring_params(&path), match_path_with_path_regex(&path, &path_regex));
+
+	path_regex = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi");
+	path       = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi");
+	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&path_regex), printf_dstring_params(&path), match_path_with_path_regex(&path, &path_regex));
+
+	path_regex = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi/*");
+	path       = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi");
+	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&path_regex), printf_dstring_params(&path), match_path_with_path_regex(&path, &path_regex));
+
+	path_regex = get_dstring_pointing_to_literal_cstring("/rohan/*/dvivedi");
+	path       = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi");
+	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&path_regex), printf_dstring_params(&path), match_path_with_path_regex(&path, &path_regex));
+
+	path_regex = get_dstring_pointing_to_literal_cstring("/*/vipulkumar/dvivedi");
+	path       = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi");
+	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&path_regex), printf_dstring_params(&path), match_path_with_path_regex(&path, &path_regex));
+
+	path_regex = get_dstring_pointing_to_literal_cstring("*/rohan/vipulkumar/dvivedi");
+	path       = get_dstring_pointing_to_literal_cstring("/rohan/vipulkumar/dvivedi");
+	printf(printf_dstring_format " == " printf_dstring_format " => %d\n", printf_dstring_params(&path_regex), printf_dstring_params(&path), match_path_with_path_regex(&path, &path_regex));
 }
