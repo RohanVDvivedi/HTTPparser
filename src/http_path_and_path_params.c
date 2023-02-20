af8259017565d68b8a077fabbf74cd4474a109ef
RohanVDvivedi <rohandvivedi@gmail.com>
2023-02-19 22:13:08 -0700

    minor fix

--- src/http_path_and_path_params.c
+++ src/http_path_and_path_params.c
@@ -107,9 +107,16 @@ int parse_url_encoded_param(stream* rs, dstring* key, dstring* value, int is_fir
 	int last_byte;
 
 	dstring key_encoded = read_until_any_end_chars_from_stream(rs, is_end_char_for_param_key, NULL, &last_byte, 2048, &error);
-	if(error || is_empty_dstring(&key_encoded))
+	if(error)
 	{
 		deinit_dstring(&key_encoded);
+		return -1;
+	}
+	if(is_empty_dstring(&key_encoded))
+	{
+		deinit_dstring(&key_encoded);
+		if(is_first_param)
+			return -2;
 		return -1;
 	}
 
