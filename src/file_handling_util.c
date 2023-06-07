75e5b3cb1b9019b4825e9323cdf4e610a926999a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-06 21:20:46 -0700

    --

--- src/file_handling_util.c
+++ src/file_handling_util.c
@@ -4,10 +4,10 @@
 
 dstring get_extension_from_file_path(const dstring* file_path)
 {
-	unsigned int ext_len = 0;
+	size_t ext_len = 0;
 
 	const char* file_path_str = get_byte_array_dstring(file_path);
-	unsigned int file_path_len = get_char_count_dstring(file_path);
+	size_t file_path_len = get_char_count_dstring(file_path);
 
 	while(ext_len < file_path_len && file_path_str[file_path_len - 1 - ext_len] != '.')
 		ext_len++;
