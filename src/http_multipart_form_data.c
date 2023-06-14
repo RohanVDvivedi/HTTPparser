da52b597c2809e4211b90c305fc8829e0a18f59e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-13 23:38:52 -0700

    added a function to read prefic of multi part form data

--- src/http_multipart_form_data.c
+++ src/http_multipart_form_data.c
@@ -5,7 +5,15 @@
 // return -1 if boundary is not read
 int read_prefix_multipart_form_data(stream* strm, const dstring* boundary)
 {
+	if(is_empty_dstring(boundary))
+		return -1;
 
+	int error = 0;
+	size_t bytes_read = skip_dstring_from_stream(strm, boundary, &error);
+	if(error || bytes_read == 0)
+		return -1;
+
+	return 0;
 }
 
 static multipart_form_data_segment* new_multipart_form_data_segment()
