7a008af25e76d3d459448c9956ad48505240a22a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-14 03:36:41 -0700

    multipart form data parseing bug fixes

--- src/http_multipart_form_data.c
+++ src/http_multipart_form_data.c
@@ -8,7 +8,8 @@
 
 #include<stdlib.h>
 
-const dstring MULTIPART_FORM_DATA_end = get_dstring_pointing_to_literal_cstring("--");
+const dstring __MULTIPART_FORM_DATA = get_dstring_pointing_to_literal_cstring("--");
+const dstring MULTIPART_FORM_DATA_end = get_dstring_pointing_to_literal_cstring("--\r\n");
 
 // return -1 if boundary is not read
 int read_prefix_multipart_form_data(stream* strm, const dstring* boundary)
@@ -18,7 +19,7 @@ int read_prefix_multipart_form_data(stream* strm, const dstring* boundary)
 
 	dstring __boundary;
 	init_empty_dstring(&__boundary, get_char_count_dstring(boundary) + 2);
-	concatenate_dstring(&__boundary, &MULTIPART_FORM_DATA_end);
+	concatenate_dstring(&__boundary, &__MULTIPART_FORM_DATA);
 	concatenate_dstring(&__boundary, boundary);
 
 	int error = 0;
@@ -47,21 +48,8 @@ multipart_form_data_segment* parse_next_multipart_form_data(stream* strm, const 
 	size_t bytes_read = skip_dstring_from_stream(strm, &MULTIPART_FORM_DATA_end, error);
 	if((*error))
 		return NULL;
-
-	if(bytes_read == 0)
-		goto CHECK_CRLF_BEFORE_HEADER;
-
-	char byte;
-	size_t byte_read = read_from_stream(strm, &byte, 1, error);
-	if((*error) || byte_read == 0)
+	if(bytes_read > 0)
 		return NULL;
-	else
-	{
-		(*error) = -1;
-		return NULL;
-	}
-
-	CHECK_CRLF_BEFORE_HEADER:;
 
 	bytes_read = skip_dstring_from_stream(strm, &CRLF, error);
 	if((*error))
@@ -83,7 +71,7 @@ multipart_form_data_segment* parse_next_multipart_form_data(stream* strm, const 
 	dstring __boundary;
 	init_empty_dstring(&__boundary, get_char_count_dstring(boundary) + 4);
 	concatenate_dstring(&__boundary, &CRLF);
-	concatenate_dstring(&__boundary, &MULTIPART_FORM_DATA_end);
+	concatenate_dstring(&__boundary, &__MULTIPART_FORM_DATA);
 	concatenate_dstring(&__boundary, boundary);
 
 	if(!initialize_stream_for_reading_until_dstring(&(seg->body_stream), strm, &__boundary))
