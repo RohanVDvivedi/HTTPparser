6f6ebb402861b19ebb422ae3f6a69e1804d4e5d8
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-14 07:05:04 -0700

    added more comments and a declaration to a necessary method

--- inc/http_multipart_form_data.h
+++ inc/http_multipart_form_data.h
@@ -7,17 +7,31 @@
 typedef struct multipart_form_data_segment multipart_form_data_segment;
 struct multipart_form_data_segment
 {
+	// headers that go along with it
 	dmap headers;
 
+	// actual contents of the body of multipart form daa
 	stream body_stream;
 };
 
-// return -1 if boundary is not read
+// return -1 if "--boundary" is not read
+// this function must be called first on the stream (on top of body stream and decoding streams)
 int read_prefix_multipart_form_data(stream* strm, const dstring* boundary);
 
 // returns NULL if not new segment present, error will be set on an error
+// the contents of the file/field will be inside the body_stream of the returned multipart_form_segement
+// you may call this function again and again (as many times as the number of fields) until it returns NULL
+// you must call this function only after reading all the contents of the previous segment
 multipart_form_data_segment* parse_next_multipart_form_data(stream* strm, const dstring* boundary, int* error);
 
+#define NAME_PRESENT 			0b01
+#define FILENAME_PRESENT 		0b10
+#define NAME_n_FILENAME_PRESENT 0b11
+
+// TODO
+int get_name_n_filename_from_content_disposition_header(const multipart_form_data_segment* seg, dstring* name, dstring* filename);
+
+// you must call destroy function on any of multipart_form_data_segment returned by parse_next_multipart_form_data
 void destroy_multipart_form_data_segment(multipart_form_data_segment* seg);
 
 #endif
