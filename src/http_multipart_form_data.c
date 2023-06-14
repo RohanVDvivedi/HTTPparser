26faad24c143e14899da0b3e7331cfe021a29111
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-14 01:40:19 -0700

    implemented parser for http multipart form data

--- src/http_multipart_form_data.c
+++ src/http_multipart_form_data.c
@@ -1,6 +1,11 @@
 #include<http_multipart_form_data.h>
 
+#include<stream_util.h>
+#include<read_until_dstring_stream.h>
+
 #include<stdlib.h>
+
+#include<http_headers.h>
 
 // return -1 if boundary is not read
 int read_prefix_multipart_form_data(stream* strm, const dstring* boundary)
@@ -23,9 +28,46 @@ static multipart_form_data_segment* new_multipart_form_data_segment()
 	return seg;
 }
 
+const dstring MULTIPART_FORM_DATA_end = get_dstring_pointing_to_literal_cstring("--");
+
 multipart_form_data_segment* parse_next_multipart_form_data(stream* strm, const dstring* boundary, int* error)
 {
+	(*error) = 0;
 
+	size_t bytes_read = skip_dstring_from_stream(strm, &MULTIPART_FORM_DATA_end, error);
+	if((*error))
+		return NULL;
+
+	if(bytes_read == 0)
+		goto PARSE_HEADER;
+
+	char byte;
+	size_t byte_read = read_from_stream(strm, &byte, 1, error);
+	if((*error) || byte_read == 0)
+		return NULL;
+
+	// unread all of the contents that we read until now
+	unread_from_stream(strm, &byte, 1);
+	unread_dstring_from_stream(strm, &MULTIPART_FORM_DATA_end);
+
+	PARSE_HEADER:;
+
+	multipart_form_data_segment* seg = new_multipart_form_data_segment();
+	if(-1 == ((*error) = parse_http_headers(strm, &(seg->headers))))
+	{
+		deinit_dmap(&(seg->headers));
+		free(seg);
+		return NULL;
+	}
+
+	if(!initialize_stream_for_reading_until_dstring(&(seg->body_stream), strm, boundary))
+	{
+		deinit_dmap(&(seg->headers));
+		free(seg);
+		return NULL;
+	}
+
+	return seg;
 }
 
 void destroy_multipart_form_data_segment(multipart_form_data_segment* seg)
