eb5afda4c836d1e3a61ecf5c560ba406700d7ccd
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-17 11:21:03 -0700

    minor bug fixes

--- src/uri_parser.c
+++ src/uri_parser.c
@@ -36,16 +36,16 @@ int parse_uri(uri* uri_p, const dstring* uri_val)
 
 		cy_uint authority_end_pos = get_char_count_dstring(&uri_val_pdstr);
 		cy_uint f_slsh_pos = contains_dstring_RK(&uri_val_pdstr, &F_SLSH);
-		authority_end_pos = min(f_slsh, authority_end_pos);
+		authority_end_pos = min(f_slsh_pos, authority_end_pos);
 		cy_uint qm_pos = contains_dstring_RK(&uri_val_pdstr, &F_SLSH);
 		authority_end_pos = min(qm_pos, authority_end_pos);
 		cy_uint pnd_pos = contains_dstring_RK(&uri_val_pdstr, &PND);
 		authority_end_pos = min(pnd_pos, authority_end_pos);
 
-		if(f_slsh_pos != INVALID_INDEX && ((qm_pos != INVALID_INDEX && f_slsh_pos >= qm_pos) || (pnd_pos != INVALID_INDEX && f_slsh_pos < pnd_pos))
+		if(f_slsh_pos != INVALID_INDEX && ((qm_pos != INVALID_INDEX && f_slsh_pos > qm_pos) || (pnd_pos != INVALID_INDEX && f_slsh_pos > pnd_pos)))
 			return -1;
 
-		if(qm_pos != INVALID_INDEX && ((pnd_pos != INVALID_INDEX && qm_pos >= pnd_pos))
+		if(qm_pos != INVALID_INDEX && ((pnd_pos != INVALID_INDEX && qm_pos > pnd_pos)))
 			return -1;
 
 		// this will contain "userinfo @ host : port"
