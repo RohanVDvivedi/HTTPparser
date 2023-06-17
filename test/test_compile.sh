f54d8f5300e1b07b0bc391c578126f14eb0a1c23
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-17 10:45:44 -0700

    added test case for uri parser

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -5,3 +5,5 @@ gcc ./test_c.c -o test_c.out -lhttpparser -lconnman -lcutlery -lssl -lz
 gcc ./test_file_handling_util.c -o test_file_handling_util.out -lhttpparser -lcutlery
 
 gcc ./test_t.c -o test_t.out -lhttpparser -lconnman -lcutlery
+
+gcc ./test_up.c -o test_up.out -lhttpparser -lconnman -lcutlery
---
+++ test/test_up.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<uri_parser.h>
+
+int main(int argc, char** argv)
+{
+	if(argc != 2)
+	{
+		printf("invalid argument count, it must be 2\n");
+		return -1;
+	}
+
+	uri uriv;
+	init_uri(&uriv);
+	int res = parse_uri(&uriv);
+	if(res)
+		printf("error parsing the uri\n");
+	else
+	{
+		printf("scheme : < " printf_dstring_format " >", printf_dstring_params(&(uriv.scheme)));
+		printf("userinfo : < " printf_dstring_format " >", printf_dstring_params(&(uriv.userinfo)));
+		printf("host : < " printf_dstring_format " >", printf_dstring_params(&(uriv.host)));
+		printf("port : < " printf_dstring_format " >", printf_dstring_params(&(uriv.port)));
+		printf("path : < " printf_dstring_format " >", printf_dstring_params(&(uriv.path)));
+		printf("query : < " printf_dstring_format " >", printf_dstring_params(&(uriv.query)));
+		printf("fragment : < " printf_dstring_format " >", printf_dstring_params(&(uriv.fragment)));
+	}
+	deinit_uri(&uriv);
+	return res;
+}
