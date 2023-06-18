5f9a0bbb52e12bb8dce8e03008eb92f08faf2207
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-17 22:25:57 -0700

    --

--- inc/http_path_and_path_params.h
+++ inc/http_path_and_path_params.h
@@ -25,6 +25,7 @@ int serialize_url_encoded_params(stream* ws, const dmap* params);
 // internal function exposed api
 
 // use this only if you know what you are doing
+// returns 1 for success and 0 for an error
 int uri_to_dstring_format(const dstring* str, dstring* res);
 
 #endif
