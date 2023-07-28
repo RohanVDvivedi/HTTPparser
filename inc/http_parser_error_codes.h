cbe3b70e792c29edc3aef0fb37c25c2c76a42926
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-28 05:02:27 -0700

    added additonal error code to be used with http status serializer

--- inc/http_parser_error_codes.h
+++ inc/http_parser_error_codes.h
@@ -3,9 +3,10 @@
 
 // This are the error codes to be used with http parser and serializer of this library
 
-#define HTTP_NO_ERROR          0
-#define HTTP_PARSER_ERROR     -1
-#define HTTP_ERROR_IN_STREAM  -2
-#define HTTP_ALLOCATION_ERROR -3
+#define HTTP_NO_ERROR              0
+#define HTTP_PARSER_ERROR         -1
+#define HTTP_ERROR_IN_STREAM      -2
+#define HTTP_ALLOCATION_ERROR     -3
+#define HTTP_OBJECT_INVALID_ERROR -4
 
 #endif
