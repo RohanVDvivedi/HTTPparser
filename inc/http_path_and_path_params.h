394fb6019fbd36f5341ed570e05ae72c59267e34
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-28 05:45:34 -0700

    added comments to define the new api for http path and path params

--- inc/http_path_and_path_params.h
+++ inc/http_path_and_path_params.h
@@ -3,23 +3,26 @@
 
 #include<http_request.h>
 
-// returns -1 for error and 0 for success
+#include<http_parser_error_codes.h>
+
+// returns int value, suggesting error, from error codes given in http_parser_error_codes.h
 int parse_http_path_and_path_params(stream* rs, http_request_head* hr_p);
 
-// returns -1 for error and 0 for success
+// returns int value, suggesting error, from error codes given in http_parser_error_codes.h
 int serialize_http_path_and_path_params(stream* ws, const http_request_head* hr_p);
 
-// key and value will be initialized by this function, you only need to deinit_dstring them
-// and after this call they will contain the next key value pair, unless there is an error
-// an error of -2 implies an end of params, while -1 implies a generic parsing error in message format
+// returns int value, suggesting error, from error codes given in http_parser_error_codes.h
+// an error of HTTP_OBJECT_INVALID_ERROR implies an end of params
 int parse_url_encoded_param(stream* rs, dstring* key, dstring* value, int is_first_param);
 
-// this function never returns -1, hence no error
+// returns int value, suggesting error, from error codes given in http_parser_error_codes.h
 int parse_url_encoded_params(stream* rs, dmap* params);
 
+// returns int value, suggesting error, from error codes given in http_parser_error_codes.h
 // in the below functions set the is_first_param flag if this is the first param being written to the stream
 int serialize_url_encoded_param(stream* ws, const dstring* key, const dstring* value, int is_first_param);
 
+// returns int value, suggesting error, from error codes given in http_parser_error_codes.h
 int serialize_url_encoded_params(stream* ws, const dmap* params);
 
 // internal function exposed api
